5b1d9648f194b63017e475afc6bd3882b9069715
Hung Nguyen <hung135@hotmail.com>
2022-10-31 17:09:38 -0400

    update params

--- ConvertDocm/Program.cs
+++ ConvertDocm/Program.cs
@@ -6,8 +6,24 @@ using System.IO.Packaging;
 class Program {
 
     static void Main(string[] args) {
-        string filename = @"C:\Users\Public\Documents\WithMacros.docm";
+        //string filename = @"C:\Users\Public\Documents\WithMacros.docm";
+        string filename = @"./example.docm";
+        if (args.Length>0)
+        {
+
+        Console.Clear();
+        Console.WriteLine("Start");
+        Console.WriteLine(args[0]);
+        filename=args[0];
+        Console.WriteLine(filename);
         ConvertDOCMtoDOCX(filename);
+        Console.WriteLine("End");
+        }
+        else{
+            Console.WriteLine("Please provide path to DOCM file");
+        }
+
+                   
     }
 
     public static void ConvertDOCMtoDOCX(string fileName)
@@ -19,11 +35,13 @@ class Program {
         {
             // Access the main document part.
             var docPart = document.MainDocumentPart;
-            
+            Console.WriteLine("Inside 1");
             // Look for the vbaProject part. If it is there, delete it.
             var vbaPart = docPart.VbaProjectPart;
+            Console.WriteLine(vbaPart);
             if (vbaPart != null)
             {
+                Console.WriteLine("Inside 2");
                 // Delete the vbaProject part and then save the document.
                 docPart.DeletePart(vbaPart);
                 docPart.Document.Save();
@@ -36,6 +54,10 @@ class Program {
                 // Track that the document has been changed.
                 fileChanged = true;
             }
+            else 
+            {
+            Console.WriteLine("No VBA found");
+            }
         }
 
         // If anything goes wrong in this file handling,
@@ -44,7 +66,8 @@ class Program {
         {
             // Create the new .docx filename.
             var newFileName = Path.ChangeExtension(fileName, ".docx");
-            
+            Console.Clear();
+            Console.WriteLine("Standard Numeric Format Specifiers");
             // If it already exists, it will be deleted!
             if (File.Exists(newFileName))
             {
